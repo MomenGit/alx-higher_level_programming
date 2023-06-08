89c10a10458740103edbdb12e4e9c272231dc779
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-08 19:11:33 +0300

    Write 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argv.pop(0)
+    sum = 0
+    if (len(argv)):
+        for arg in argv:
+            sum = sum + int(arg)
+    print(sum)
