4783d734c91d87989f567ed693b31db0c27d6eaa
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-08 19:05:02 +0300

    Write 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argv.pop(0)
+    if (len(argv) == 0):
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(len(argv)))
+        for i, arg in enumerate(argv, 1):
+            print("{:d}: {:s}".format(i, arg))
