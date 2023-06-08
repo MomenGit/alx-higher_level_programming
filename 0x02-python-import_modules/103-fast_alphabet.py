f6d7b61286f859b866bce8b8b0a83e133e74d58e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-08 21:21:16 +0300

    Write 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+print(str(list(map(chr, range(65, 91)))).replace(
+    chr(39), chr(0)).replace(chr(32), chr(0)).replace(chr(44), chr(0))
+    .replace(chr(91), chr(0)).replace(chr(93), chr(0)))
