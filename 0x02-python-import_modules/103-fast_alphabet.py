6d99f106a09729f2e52f1063c2f470ac6b841bb5
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-08 21:25:42 +0300

    Make it easier and faster

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-print(str(list(map(chr, range(65, 91)))).replace(
-    chr(39), chr(0)).replace(chr(32), chr(0)).replace(chr(44), chr(0))
-    .replace(chr(91), chr(0)).replace(chr(93), chr(0)))
+print(*(list(map(chr, range(65, 91)))), sep='')
