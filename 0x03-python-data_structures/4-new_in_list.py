61d062f485a0c55d26e77d0a946e5be352029a09
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-11 15:49:14 +0300

    Write 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    for i in my_list:
+        new_list.append(i)
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    new_list.pop(idx)
+    new_list.insert(idx, element)
+    return new_list
