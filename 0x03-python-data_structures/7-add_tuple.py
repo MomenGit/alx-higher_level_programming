2fcbc04b3c9bbc2b69a1b59647549b7e3386f0d8
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-11 18:37:29 +0300

    Write 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup = [0, 0]
+
+    for i in range(2):
+        try:
+            tup[i] = tup[i]+tuple_a[i]
+        except IndexError:
+            pass
+        try:
+            tup[i] = tup[i]+tuple_b[i]
+        except IndexError:
+            pass
+
+    return (tup[0], tup[1])
