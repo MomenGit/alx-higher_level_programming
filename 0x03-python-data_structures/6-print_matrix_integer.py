32363df8674f88a1d0b8bf3f4400210c586fffe1
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-11 16:28:45 +0300

    Write 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,9 @@
-#!/usr/bin
-/python3
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        row_len = len(matrix[i])
+        for j in range(row_len):
+            print("{:d}".format(matrix[i][j]), end='')
+            if j < row_len-1:
+                print(" ", end='')
+        print("")
