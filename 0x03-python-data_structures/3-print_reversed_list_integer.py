e63facaf852d3ac203e43b556308c2c2bdbe9434
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-11 15:52:47 +0300

    Add null safety

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     end = len(my_list)-1
     for i in range(end, -1, -1):
         print("{:d}".format(my_list[i]))
