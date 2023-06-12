a776bad150e1d15bce6873fe4a0575deb41b5107
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-12 14:50:11 +0300

    Write 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,24 @@
+#include <Python.h>
+#include <stdio.h>
 
-void print_python_list_info(PyObject *p){
+/**
+ * print_python_list_info - prints prints some basic info about Python lists
+ *
+ * @p: python object
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t list_len = 0;
 
+	if (!PyList_Check(p))
+		return;
+
+	list_len = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %li\n", list_len);
+	printf("[*] Allocated = %li\n", ((PyListObject *)p)->allocated);
+	for (Py_ssize_t i = 0; i < list_len; i++)
+	{
+		printf("Element %li: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
