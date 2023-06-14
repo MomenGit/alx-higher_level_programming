05955c777e52ab9c9227c5818313a1ebcb609199
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-14 19:04:04 +0300

    Write 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary)
+    print("Number: %d".format(len(sorted_keys)))
+    for i in sorted_keys:
+        print("{0}: {1}".format(i, a_dictionary[i]))
