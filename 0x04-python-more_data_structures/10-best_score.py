74e357b90aea04b45bde71da13ab847931524118
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-14 21:14:26 +0300

    Write 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    values = list(a_dictionary.items())
+    best_scorer = values[0]
+    for i in values:
+        best_scorer = i if i[1] > best_scorer[1] else best_scorer
+
+    return best_scorer[0]
