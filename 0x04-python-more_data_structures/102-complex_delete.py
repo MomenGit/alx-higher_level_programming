e55ae5eb3439a4707ad444aa757f5661df2950f5
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-14 23:54:42 +0300

    Write 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    items = a_dictionary.copy().items()
+
+    for item in items:
+        if item[1] == value:
+            del a_dictionary[item[0]]
+    return a_dictionary
