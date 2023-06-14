edce02a7db6d8aa6aa92e4f9048dbbf86d24c738
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-14 23:43:19 +0300

    Write 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    avg = 0
+    weights = 0
+    for tup in my_list:
+        avg = avg + tup[0] * tup[1]
+        weights = weights + tup[1]
+
+    return avg / weights
