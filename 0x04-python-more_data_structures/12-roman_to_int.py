2e999bf8a6792fdebea193dd158d3cd9d79c2ea1
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-14 23:22:01 +0300

    Fix rsubtractive forms

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,16 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
+    number = 0
     subtractive_forms = {'IV': 4, 'IX': 9,
                          'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-    if roman_string in subtractive_forms:
-        return subtractive_forms[roman_string]
-
-    number = 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for form in subtractive_forms:
+        if form in roman_string:
+            number = number + subtractive_forms[form]
+            roman_string = roman_string.replace(form, "")
+
     for i in roman_string:
         number = number + romans[i]
 
