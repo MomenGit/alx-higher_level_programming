6c59909cfae430c9d0cc5c17d6558f9717f4ad38
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-23 16:17:14 +0300

    Write check_cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,22 @@
 #include "lists.h"
 
+/**
+ * check_cycle - ...
+ *
+ * @list: ...
+ * Return: int
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *tmp_node;
+
+	tmp_node = list;
+	while (tmp_node != NULL)
+	{
+		if (tmp_node->next != list)
+			tmp_node = tmp_node->next;
+		else
+			return (1);
+	}
 	return (0);
 }
-
