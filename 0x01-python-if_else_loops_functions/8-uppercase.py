a5ea90cfe877bd283ea5405300975072b853bd3a
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-25 18:45:06 +0300

    Remove testing code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,3 @@ def uppercase(str):
         print("{:s}".format((chr(ord(c)-32))
               if (c >= 'a' and c <= 'z') else c), end='')
     print()
-
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
