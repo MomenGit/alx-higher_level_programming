def7996a823d52f5c947c9e711e3267ca66d0adc
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-25 18:31:07 +0300

    Write 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    number = number if number >= 0 else -number
+    last_digit = number % 10
+    print(last_digit, end='')
+    return (last_digit)
