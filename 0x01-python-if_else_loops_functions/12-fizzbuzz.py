feaa33e366736b7cb835805b585dd078e2389be0
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-25 18:23:33 +0300

    Write 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if not ((i % 3) or (i % 5)):
             print("FizzBuzz", end=' ')
-        if not (i % 3):
+        elif not (i % 3):
             print("Fizz", end=' ')
         elif not (i % 5):
             print("Buzz", end=' ')
