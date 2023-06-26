e26726ed78e084c2b67aa791e276b6400bcc7821
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-26 14:26:32 +0300

    Make exception of all errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print(f"{my_list[i]}", end='')
         i = i+1
-    except IndexError:
+    except:
         pass
     print()
     return i
