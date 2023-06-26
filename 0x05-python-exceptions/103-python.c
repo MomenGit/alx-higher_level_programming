65196a583c95faba4bfecab39bc724f4ab8a784e
momengit <51290928+MomenGit@users.noreply.github.com>
2023-06-26 11:23:44 +0300

    Prepare project files

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+/**
+ * print_python_list -  print some basic info about Python lists
+ *
+ * @p: a PyObject of type python list
+ */
+void print_python_list(PyObject *p) {}
+
+/**
+ * print_python_bytes -  print some basic info about Python bytes
+ *
+ * @p: a PyObject of type python bytes
+ */
+void print_python_bytes(PyObject *p) {}
+
+/**
+ * print_python_float -  print some basic info about Python floats
+ *
+ * @p: a PyObject of type python float
+ */
+void print_python_float(PyObject *p) {}
