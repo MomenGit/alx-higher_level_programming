b71f79b08b81e309ee74712e255e78b76b7354bf
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-02 18:01:51 +0300

    Fix pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,22 +10,22 @@ class Square:
     Attributes:
         size (int): The size of the square
             (defaults to 0 must be positive integer)
-        position (tuple(,)): The (x,y) position of the shape 
+        position (tuple(,)): The (x,y) position of the shape
             (defaults to (0,0) must be positive integers)
     """
 
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size: int = 0, position: tuple = (0, 0)) -> None:
         """Initialize Square"""
 
         self.__position = position
         self.__size = size
 
-    def area(self):
+    def area(self) -> int:
         """Calculate the area of the square"""
 
         return self.__size**2
 
-    def my_print(self):
+    def my_print(self) -> None:
         """Print the square shape to the stdout"""
 
         if self.__size == 0:
@@ -37,11 +37,11 @@ class Square:
             print("{}{}".format(' '*self.__position[0], '#'*self.__size))
 
     @property
-    def size(self):
+    def size(self) -> int:
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, value: int) -> None:
         if type(value) is int:
             if value < 0:
                 raise ValueError("size must be >= 0")
@@ -51,11 +51,11 @@ class Square:
             raise TypeError("size must be an integer")
 
     @property
-    def position(self):
+    def position(self) -> tuple:
         return self.__position
 
     @position.setter
-    def position(self, value):
+    def position(self, value: tuple):
         if type(value) is tuple:
             if len(value) == 2:
                 if value[0] >= 0 and value[1] >= 0:
