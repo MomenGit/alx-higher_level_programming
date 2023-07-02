91bd77e33a76d3578218bc5761140acdfa70d807
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-02 15:53:30 +0300

    Write 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square shape object
+
+    Args:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size: int = 0) -> None:
+        """Initialize Square"""
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        self.__size = size
