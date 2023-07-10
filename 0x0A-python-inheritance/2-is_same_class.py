66a71da64a0945066ae70388e05974c23aa24702
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-10 18:11:27 +0300

    Write 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Define is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """Return True if the object is exactly an instance of the specified class;
+    otherwise False
+
+    Args:
+        obj: an object instance
+        a_class: a class name
+    """
+    return type(obj) == a_class
