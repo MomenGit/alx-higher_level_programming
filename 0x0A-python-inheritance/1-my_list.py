cedb3a5deae9e3dd86710a0b5258112709e6c2da
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-10 18:07:23 +0300

    Write 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defines class MyList"""
+
+
+class MyList(list):
+    """A subclass of list"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        shallow_copy = self[:]
+        shallow_copy.sort()
+        print(shallow_copy)
