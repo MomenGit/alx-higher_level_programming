533e925518d213bb40d2eb5ec4ec587cba7a5166
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-10 15:06:39 +0300

    Write 0-add_integer.py

--- 0x07-python-test-driven_development/0-add_integer.py
+++ 0x07-python-test-driven_development/0-add_integer.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+""" Defines an add function"""
+
+
 def add_integer(a, b=98):
-    pass
+    """Adds two integers"""
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+    return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/0-add_integer.txt", verbose=True)
