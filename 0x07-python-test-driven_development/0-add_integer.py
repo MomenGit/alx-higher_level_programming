955a010b9039c7d3608e7fe4332d0988f5fe6d0f
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-10 16:11:46 +0300

    Add more tests and docstrings

--- 0x07-python-test-driven_development/0-add_integer.py
+++ 0x07-python-test-driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines an add function
-Adds integers of floats
-otherwise raises TypeError exception
-"""
+""" Defines an add function """
 
 
 def add_integer(a, b=98):
     """Adds two integers
-        a and b must be integers or floats, otherwise raise a TypeError
+
+    Args:
+        a (int|float): first number
+        b (int|float): second number
+
+    Raises:
+        TypeError: If a or b aren't instances of int or float
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
