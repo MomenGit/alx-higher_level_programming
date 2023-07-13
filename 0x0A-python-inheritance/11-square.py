bf6851ca9425423450c9a4cef2213afab015bd62
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-13 16:48:02 +0300

    Write 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,5 +4,21 @@ mod = __import__("9-rectangle")
 
 
 class Square(mod.Rectangle):
-    """Represents a Square Object"""
-    pass
+    """Represents a Square Object
+
+    Args:
+        size (int): The size of the square instance
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculate square's instance area"""
+
+        return self.__size**2
+
+    def __str__(self):
+        return "[{0}] {1}/{1}".format("Square", self.__size)
