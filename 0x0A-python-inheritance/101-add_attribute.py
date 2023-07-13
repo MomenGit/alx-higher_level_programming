34cef3f98be17631e2b2523cb7dfe0f078f0fa70
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-13 18:58:20 +0300

    Write 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Defines add_attribute function"""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """adds a new attribute to an object if it's possible"""
+
+    if not hasattr(type(obj), "__slots__") and not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, attr_name, attr_value)
