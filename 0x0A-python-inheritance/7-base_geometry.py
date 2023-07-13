01bc5e7314bc59453d9fe2f2d305beec456b95ea
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-13 16:10:43 +0300

    Add docstring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,13 +6,15 @@ class BaseGeometry:
     """Base class Geometry"""
 
     def area(self):
+        """Raises an Exception with the message
+        'area() is not implemented'"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validates value must be an integer greater than 0"""
 
-        if type(value) is int:
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
-        else:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
