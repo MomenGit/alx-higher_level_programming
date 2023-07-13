b145feab6292f037ff57c1666ac3fcd749872040
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-13 15:53:38 +0300

    Write 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines inherits from function"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
+    from the specified class; otherwise False
+
+    Args:
+        obj: An object instance
+        a_class: obj class name
+    """
+    if type(obj) == a_class:
+        return False
+
+    return issubclass(type(obj), a_class)
