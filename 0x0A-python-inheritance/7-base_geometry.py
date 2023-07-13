a68a366acb0ccbf379c6f9602df8edc05119ce7c
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-13 16:01:55 +0300

    Write 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,4 +4,17 @@
 
 class BaseGeometry:
     """Base class Geometry"""
-    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value must be an integer greater than 0"""
+
+        if isinstance(value, int):
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+            else:
+                self.__size = value
+        else:
+            raise TypeError("{} must be an integer".format(name))
