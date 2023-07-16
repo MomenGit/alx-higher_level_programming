1ec29a719a740cd1ab71932d7270e00626eb6e8c
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-16 15:53:07 +0300

    Add more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,31 +10,47 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_module_docstring(self):
         """Tests for module docstring"""
-        m = __import__('6-max_integer').__doc__
-        self.assertTrue(len(m) > 1)
+        self.assertTrue(len(__import__('6-max_integer').__doc__) > 1)
 
     def test_function_docstring(self):
         """Tests for function docstring"""
-        f = max_integer.__doc__
-        self.assertTrue(len(f) > 1)
+        self.assertTrue(len(max_integer.__doc__) > 1)
 
     def test_empty_list(self):
         """Test against an empty list"""
         self.assertIsNone(max_integer([]))
 
+    def test_no_args(self):
+        """Tests for no arguments passed to func"""
+        self.assertIsNone(max_integer())
+
+    def test_one_element(self):
+        """Tests for only one number in the list"""
+        self.assertEqual(max_integer([1]), 1)
+
     def test_default(self):
         """Test against a normal case"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
+    def test_positive_end(self):
+        """Tests for all positive with max at end"""
+        self.assertEqual(max_integer([2, 10, 8, 36, 14, 50]), 50)
+
+    def test_positive_middle(self):
+        """Tests for all positive with max in middle"""
+        self.assertEqual(max_integer([2, 10, 8, 360, 14, 50]), 360)
+
+    def test_positive_beginning(self):
+        """Tests for all positive with max at beginning"""
+        self.assertEqual(max_integer([200, 10, 8, 36, 14, 50]), 200)
+
     def test_one_negative(self):
         """Tests for list with one negative number"""
-        on = [200, 10, 8, -36, 14, 50]
-        self.assertEqual(max_integer(on), 200)
+        self.assertEqual(max_integer([200, 10, 8, -36, 14, 50]), 200)
 
     def test_all_negative(self):
         """Tests for list with all negative numbers"""
-        n = [-6, -50, -75, -1, -100]
-        self.assertEqual(max_integer(n), -1)
+        self.assertEqual(max_integer([-6, -50, -75, -1, -100]), -1)
 
     def test_type_error(self):
         """Test against a list that contains a non-number type"""
