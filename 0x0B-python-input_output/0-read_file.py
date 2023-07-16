4dcaf2bec6cb2926a39532ab0a0f49d5038a1cbf
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-16 19:00:24 +0300

    Write 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Defines read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r") as file:
+        print(file.read(), end="")
