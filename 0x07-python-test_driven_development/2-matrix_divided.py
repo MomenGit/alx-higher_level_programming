fc1792060bbc2b78d6b95c2398001bb74ca865ec
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-16 14:58:28 +0300

    Write 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    pass
+    new_matrix = []
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    validate_matrix(matrix)
+
+    for row in matrix:
+        new_matrix.append([])
+        for e in row:
+            new_matrix[-1].append(round(e/div, 2))
+
+    return new_matrix
+
+
+def validate_matrix(matrix):
+    size = len(matrix[0])
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if len(row) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        for elem in row:
+            if not isinstance(elem, int) and not isinstance(elem, float):
+                raise TypeError(
+                    "matrix must be a matrix "
+                    "(list of lists) of integers/floats")
