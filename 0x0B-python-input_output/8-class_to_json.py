7e299157609a694910e04feefdb39ffad8f85004
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-17 16:14:38 +0300

    Write 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Defines class_to_json function"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
+    dict_repr = {}
+    attributes = obj.__dict__
+
+    for attr, value in attributes.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            dict_repr[attr] = value
+
+    return dict_repr
