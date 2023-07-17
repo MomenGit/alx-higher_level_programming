5ce3c055d4fc05d9038babe3f9164ffbf074e7cd
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-17 19:21:11 +0300

    Prepare project files

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines Rectangle class"""
+from base import Base
+
+
+class Rectangle(Base):
+    """Define a rectangle object"""
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines Rectangle class"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Define a square object"""
