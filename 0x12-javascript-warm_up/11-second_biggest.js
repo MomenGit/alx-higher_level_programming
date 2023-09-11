d91534a73b77d959e08fbda86df0ca9488e7f86e
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 18:06:07 +0300

    Write ./11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+const arr = argv.slice(2);
+
+if (arr.length <= 1) console.log(0);
+
+let biggest = arr[0];
+let second = biggest;
+arr.forEach((element) => {
+  if (element > biggest) {
+    second = biggest;
+    biggest = element;
+  }
+});
+console.log(second);
