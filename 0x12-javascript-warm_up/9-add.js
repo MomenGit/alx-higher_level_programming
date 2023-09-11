a4b91bfe443464152e2d41ec316e56a58cc469a9
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 14:52:47 +0300

    Write 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+
+add(a, b);
