7974732896853f087cc6a503117044629d7165f4
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 17:45:20 +0300

    Write 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+function factorial (num) {
+  if (isNaN(num) || num === 0) return 1;
+  return num * factorial(num - 1);
+}
+
+const num = parseInt(argv[2]);
+
+console.log(factorial(num));
