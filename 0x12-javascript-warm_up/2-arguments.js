114629adedc7cf4251886e943c8eec3b2f8fa2c4
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 14:12:16 +0300

    Write 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+length = argv.length - 2;
+
+if (length === 0) {
+  console.log('No argument');
+} else if (length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
