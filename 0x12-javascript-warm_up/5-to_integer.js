009891f65fbc6adda2713ecabbfa66c6ba4f3fce
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 14:28:14 +0300

    Write 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+
+const number = +argv[2];
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', number);
+}
