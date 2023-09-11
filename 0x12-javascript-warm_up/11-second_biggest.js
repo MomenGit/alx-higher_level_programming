f7a6b2f3115bf8cc761ca8c501d921a22b58715e
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-11 18:13:24 +0300

    Write 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,16 +2,16 @@
 
 const { argv } = require('process');
 
-const arr = argv.slice(2);
+const args = argv.slice(2); // Get the command-line arguments (excluding the first two)
+const numbers = args.map(Number); // Convert the arguments to numbers
 
-if (arr.length <= 1) console.log(0);
+if (numbers.length <= 1) {
+  console.log(0); // If no arguments or only one argument is passed, print 0
+} else {
+  // Sort the numbers in descending order
+  numbers.sort((a, b) => b - a);
 
-let biggest = arr[0];
-let second = biggest;
-arr.forEach((element) => {
-  if (element > biggest) {
-    second = biggest;
-    biggest = element;
-  }
-});
-console.log(second);
+  // Find the second biggest number (at index 1)
+  const secondBiggest = numbers[1];
+  console.log(secondBiggest);
+}
