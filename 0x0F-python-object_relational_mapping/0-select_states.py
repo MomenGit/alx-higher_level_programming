bea9ac02456e24eea2658e7dbbf0801c3adca41b
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-15 20:31:02 +0300

    Fix execution when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
-import sys
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2],
-                     db=sys.argv[3], charset="utf8")
-cur = db.cursor()
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+    if len(sys.argv) != 4:
+        sys.exit('Usage: 0-select_states.py <mysql username> <mysql password>'
+                 ' <database name>')
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    db.close()
