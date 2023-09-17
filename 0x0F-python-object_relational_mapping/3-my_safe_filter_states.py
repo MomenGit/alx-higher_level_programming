5116b814d6350f54b2cbd57b4683b3bafd19f52e
Mo'men Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-17 14:09:16 +0300

    Write 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) != 5:
+        sys.exit(
+            "Usage: 3-my_safe_filter_states.py <mysql_username> " +
+            "<mysql_password> <database_name> <state_name>")
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset='utf8')
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC",
+        (sys.argv[4]+'%',))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
