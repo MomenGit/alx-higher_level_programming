0750a4884f71de6025bcedf5ae824ac3d1990b4a
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-21 13:53:51 +0300

    Write model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a State and
+an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    Defines ORM class for table `city`, with 3 columns:
+
+    Attributes:
+        id (sqlalchemy.Column(Integer)): unique identifier, primary key
+        name (sqlalchemy.Column(String)): name of city
+        state_id (sqlalchemy.Column(Integer)): foreign key
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, unique=True, nullable=False,
+                autoincrement="auto", primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
