44909e5fe8a934489e0230f84b0bee33017bcb33
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-21 11:30:32 +0300

    Write 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine, Column
+    from sqlalchemy.orm import Session
+    import sys
+
+    if len(sys.argv) != 4:
+        sys.exit(
+            "Usage: 12-model_state_update_id_2.py <mysql_username> " +
+            "<mysql_password> <database_name>")
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_recycle=3600)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.commit()
+
+    session.close()
