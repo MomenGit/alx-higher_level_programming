e7d9d744e9507443ebdedaadd301d93c84cab7bd
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-30 00:44:48 +0300

    Write 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Docs"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == '__main__':
-    import urllib
-    pass
+    import urllib.request
+    import sys
+    request = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(request) as response:
+        print(response.headers.__getitem__('X-Request-Id'))
