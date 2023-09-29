dc0c89c4c3c1406ecfca7f9c2b1651568c75c32c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-29 23:39:09 +0300

    Write 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Defines find_peak function"""
 
+
 def find_peak(list_of_integers):
-	"""finds a peak in a list of unsorted integers"""
-    pass
+    """finds a peak in a list of unsorted integers"""
+    if list_of_integers == [] or type(list_of_integers) is not list:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
