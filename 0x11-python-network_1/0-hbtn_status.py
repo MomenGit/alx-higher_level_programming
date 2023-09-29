99365e06e4871a840db152c3e8657fff0050664a
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-30 00:35:53 +0300

    Fix import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
-            as response:
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(body)))
