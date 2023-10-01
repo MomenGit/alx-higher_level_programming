c8a358130e5ff30d8a021f64a9acf97c01102242
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-01 17:27:42 +0300

    Fix priting of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a specified URL, and displays the size of the body of the response
-curl -s -w '%{size_download}\n' $1
+curl -so /dev/null -w '%{size_download}\n' "$1"
