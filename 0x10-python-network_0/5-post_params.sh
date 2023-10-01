065d2d32513a02ab5cbab186c957b6cffe1c79fb
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-01 19:40:29 +0300

    Write 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to a specified URL, and displays the body of the response
-curl -s 
+curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
