053206f4d5ffa1f17344934092ad7475b8c98614
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:17:57 +0300

    Write 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-"""Docs"""
+"""
+takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter, and
+finally displays the body of the response
+"""
 
 if __name__ == '__main__':
     import requests
-    pass
+    import sys
+    data = {
+        'email': sys.argv[2]
+    }
+    res = requests.post(sys.argv[1], data)
+    print(res.text)
