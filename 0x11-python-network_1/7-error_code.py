eae86b8c9f2de4f5db33067dfc3eca7f965ab187
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:49:17 +0300

    Write 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-"""Docs"""
+"""
+takes in a URL, sends a request and displays the body of the response
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
+"""
 
 if __name__ == '__main__':
     import requests
-    pass
+    import sys
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
