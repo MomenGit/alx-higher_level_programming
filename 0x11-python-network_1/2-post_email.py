64818bb40037101296cbcb73b3ee18c816eb4360
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:38:32 +0300

    Add utf-8 decoding to read

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     data = data.encode('ascii')
     req = request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read())
+        print(response.read().decode('utf-8'))
