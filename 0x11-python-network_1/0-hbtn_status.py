797164e6c615f7ef18b25a73aa32792070e2ec0b
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:42:32 +0300

    Change import style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == '__main__':
-    import urllib.request
-    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(request) as response:
+    from urllib import request
+    req = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
         body = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(body)))
