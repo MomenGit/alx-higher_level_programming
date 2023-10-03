83ad0015ced7273936eb52f112ec052a139cfdaf
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:37:13 +0300

    Add utf-8 decoding to read

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     req = request.Request(sys.argv[1])
     try:
         with request.urlopen(req) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))
