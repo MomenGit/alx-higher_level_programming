cbdbb8dfbf0829ea472cf71ffe33a7fc55c7a322
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-03 17:17:57 +0300

    Write 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Docs"""
+"""
+takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header
+"""
 
 if __name__ == '__main__':
     import requests
-    pass
+    import sys
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
