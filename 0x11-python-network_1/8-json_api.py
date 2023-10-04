2618cd303ee4870f910f06f4dae5d4c396275369
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-04 14:11:11 +0300

    Write 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-"""Docs"""
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
 
 if __name__ == '__main__':
     import requests
-    pass
+    import sys
+    q = sys.argv[1] if sys.argv[1] is not None else ""
+    res = requests.post('http://0.0.0.0:5000/search_user', {'q': q})
+    try:
+        json = res.json()
+        if json is None:
+            print("No result")
+        else:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+    except Exception as e:
+        print("Not a valid json")
