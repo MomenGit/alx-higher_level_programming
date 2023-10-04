a7af3bfc1dcd61a15b44d6f69f97049edf47fadf
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-04 14:35:59 +0300

    Write 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,4 +3,14 @@
 
 if __name__ == '__main__':
     import requests
-    pass
+    import sys
+    headers = {
+        "Authorization": "Bearer {}".format(sys.argv[2]),
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    res = requests.get("https://api.github.com/{}".format(sys.argv[1]))
+    try:
+        body = res.json()
+        print("{}".format(body.get('id')))
+    except Exception as e:
+        print(None)
