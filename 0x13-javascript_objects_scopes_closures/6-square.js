2da5f64d687cc7921069594b65266bad8870af19
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-11 18:03:25 +0300

    Fix printing and semistandard issues

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
-const stdout = require('process').stdout;
-const Square = require('./5-square');
+const { stdout } = require('process');
+const Square1 = require('./5-square');
 
-module.exports = class Square extends Square {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c !== undefined) {
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
           stdout.write(c);
         }
+        stdout.write('\n');
       }
     } else {
       this.print();
