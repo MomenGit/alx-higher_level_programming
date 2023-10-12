f4b1091f126b4078a47f0dfb02fdcc1ecd6ec516
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-12 12:01:13 +0300

    Write 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occurrences = 0;
+  list.forEach((element) => {
+    if (element === searchElement) occurrences += 1;
+  });
+  return occurrences;
+};
