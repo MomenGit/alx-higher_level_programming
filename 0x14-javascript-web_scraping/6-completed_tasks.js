dddcafe4dc70cc4d9c3ec16e0efcf29ca487929c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-24 13:56:59 +0300

    Write 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id
+const request = require('request');
+const process = require('process');
+
+const usersCompleted = {};
+function computeCompleted(todos) {
+  for (const todo of todos) {
+    if (usersCompleted[todo.userId]) {
+      usersCompleted[todo.userId] += 1;
+    } else {
+      usersCompleted[todo.userId] = 1;
+    }
+  }
+}
+
+request(`${process.argv[2]}?completed=true`, (err, res, body) => {
+  if (err) console.log(err);
+  else computeCompleted(JSON.parse(body));
+  console.log(usersCompleted);
+});
