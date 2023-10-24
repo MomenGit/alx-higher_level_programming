9fde136a2d0780e1110ca96feb7faf84aded4438
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-24 10:55:29 +0300

    Write 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// reads and prints the content of a file
+const fs = require('fs');
+const { argv } = require('process');
+
+fs.readFile(argv[2], 'utf-8', function (err, data) {
+  console.log(err || data);
+});
