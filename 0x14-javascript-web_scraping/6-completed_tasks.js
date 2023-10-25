1ff6105304caebb9c1d98b24edd0a5504fcbfb58
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-25 14:43:40 +0300

    Remove completed query parameter and use if statement to check completion

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,15 +6,17 @@ const process = require('process');
 const usersCompleted = {};
 function computeCompleted (todos) {
   for (const todo of todos) {
-    if (usersCompleted[todo.userId]) {
-      usersCompleted[todo.userId] += 1;
-    } else {
-      usersCompleted[todo.userId] = 1;
+    if (todo.completed === true) {
+      if (usersCompleted[todo.userId]) {
+        usersCompleted[todo.userId] += 1;
+      } else {
+        usersCompleted[todo.userId] = 1;
+      }
     }
   }
 }
 
-request(`${process.argv[2]}?completed=true`, (err, res, body) => {
+request(`${process.argv[2]}`, (err, res, body) => {
   if (err) console.log(err);
   else computeCompleted(JSON.parse(body));
   console.log(usersCompleted);
