5b84bdca0a24cfcfcf8f9525a9f05112f8dbcd6f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-11-02 01:09:46 +0200

    Write 6-script.js

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+updates the text of the <header> element to New Header!!!
+when the user clicks on DIV#update_header
+*/
+
+$('div#update_header').click(() => {
+  $('header').text('New Header!!!');
+});
